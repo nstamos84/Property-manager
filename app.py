7e9e0ea710e99c4c2a2b17cd8653f8247936548d
nstamos84 <nstamos84@gmail.com>
2026-03-01 18:51:45 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -608,5 +608,5 @@ setInterval(loadMessages, 15000); // auto-refresh every 15s
 """
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port, debug=False)
